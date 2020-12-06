40c889a31b97262c9936c04476f3ab12853c2f0f
farnoodlotfali <farnoodlotfali@gmail.com>
2020-12-06 11:49:12 +0330

    compelete sidebar component

---
+++ src/components/SidebarChannels.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SidebarChannels.css';
+
+const SidebarChannels = ({ id, channel }) => {
+  return (
+    <div className='sidebarChannels'>
+      <h4>
+        <span className='sidebarChannels__hash'>#</span> Youtube
+      </h4>
+    </div>
+  );
+};
+
+export default SidebarChannels;
